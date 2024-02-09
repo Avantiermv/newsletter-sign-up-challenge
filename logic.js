7487be35d5b7d603bf2fa7ee6c616b10719443a0
marcos vinicius <avantiermv@gmail.com>
2024-02-09 16:46:25 -0300

    Starting with js.

--- logic.js
+++ logic.js
@@ -1,14 +1,18 @@
-let email = document.getElementById('iemail');
+let emailInput = document.getElementById('iemail');
 let button = document.getElementById('ibutton');
-let form= document.querySelector('form');
-
-
+let form = document.querySelector('form');
 
 form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-    if(email == ''){
-        console.log('1');
+    if(emailInput.value === ""){
+        emailInput.style.borderColor = 'red';
+        window.alert('error');
+    } else {
+        emailInput.style.borderColor = '';
+        window.alert('Deu certo');
     }
 
-    e.preventDefault();
+
+    
 });
