37620f8f1890bb5b3b7f2fce007e55a27473caed
marcos vinicius <avantiermv@gmail.com>
2024-02-10 10:25:57 -0300

    finished the js.

--- logic.js
+++ logic.js
@@ -1,18 +1,30 @@
 let emailInput = document.getElementById('iemail');
 let button = document.getElementById('ibutton');
 let form = document.querySelector('form');
+let span = document.querySelector('span');
+let content = document.getElementById('all');
+let thanks = document.getElementById('thanks')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    if(emailInput.value === ""){
+    if(emailInput.value == "" || IsEmailValid(emailInput.value) == false){
         emailInput.style.borderColor = 'red';
-        window.alert('error');
+        span.style.color = 'red';
+        span.style.display = 'block';
     } else {
-        emailInput.style.borderColor = '';
-        window.alert('Deu certo');
+        content.style.display = 'none';
+        thanks.style.display = 'block';
     }
 
 
-    
+    function IsEmailValid(email){
+        const emailValid = new RegExp(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi);
+
+        if(emailValid.test(email)){
+            return true;
+        } else {
+            return false;
+        }
+    }
 });
